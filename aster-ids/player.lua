74b93008e5bcc18b3b84eeae7adb26a01aeb95b4
SherlockSolastice <alecdwm@gmail.com>
2014-08-10 14:43:56 +1000

    Added player movement

--- aster-ids/player.lua
+++ aster-ids/player.lua
@@ -16,9 +16,8 @@ local thrustverts =	{	-05,-10,
 function player:create()
 	local newObj = {}
 	newObj.position = vector(love.graphics.getWidth()/2,love.graphics.getHeight()/2)
-	newObj.rotation = math.pi
+	newObj.rotation = 3*math.pi/2
 	newObj.velocity = vector(0,0)
-	newObj.mass = 100
 	newObj.thrust = 120
 	newObj.spin = 6
 	newObj.dragFactor = 0.9
@@ -34,27 +33,34 @@ function player:update(dt)
 	---- Take Player Input
 	-- Ship Turning Control
 	if love.keyboard.isDown("a") or love.keyboard.isDown("left") then
+		-- Turn left
 		self.rotation = self.rotation - self.spin * dt
 	end
 	if love.keyboard.isDown("d") or love.keyboard.isDown("right") then
+		-- Turn right
 		self.rotation = self.rotation + self.spin * dt
 	end
 	-- Acceleration/Deceleration Control
 	if love.keyboard.isDown("w") or love.keyboard.isDown("up") then
+		-- Effects
 		self.drawshape1_active = true
 		audio.thrust:play()
+
+		-- Calculations
+		self.velocity.x = self.velocity.x + math.cos(self.rotation) * self.thrust * dt
+		self.velocity.y = self.velocity.y + math.sin(self.rotation) * self.thrust * dt
 	else
+		-- Effects
 		self.drawshape1_active = false
 		audio.thrust:stop()
 	end
 
 	---- Move Ship
-	--self.position = self.position + self.velocity:rotated(self.rotation) * dt
-	--self.position = self.position + self.velocity * dt
+	self.position = self.position + self.velocity * dt
 
 	---- Apply Environmental Constraints
 	-- Off Screen
-	--[[if self.position.x > love.graphics.getWidth() then
+	if self.position.x > love.graphics.getWidth() then
 		self.position.x = 0
 	end
 	if self.position.y > love.graphics.getHeight() then
@@ -65,7 +71,7 @@ function player:update(dt)
 	end
 	if self.position.y < 0 then
 		self.position.y = love.graphics.getHeight()
-	end]]
+	end
 end
 
 function player:draw()
@@ -73,7 +79,7 @@ function player:draw()
 
 	love.graphics.push()
 	love.graphics.translate(x,y)
-	love.graphics.rotate(self.rotation)
+	love.graphics.rotate(self.rotation-math.pi/2)
 	if self.drawshape1_active then
 		show = math.random()
 		if show < 0.08 then
