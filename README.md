6365f3fe34730c4075ceeb8b63554a78fe151e68
alecdwm <alecdwm@gmail.com>
2015-01-24 18:45:10 +1000

    Patched name Updated description Converted tabs to spaces in main.lua and conf.lua Updated t.author Added window icon Switched off AA

--- README.md
+++ README.md
@@ -1,17 +1,17 @@
-# Aster-ids
+# LuaAsteroids
 
 ## Description
-Asteroids for Löve2D
+A clone of Atari's 1979 *Asteroids*, written in Lua using the Löve2D framework.
 
 ## Bugs
-*See GitHub [issue tracker](https://github.com/SherlockSolastice/Aster-ids/issues)*
+*See the GitHub [issue tracker](https://github.com/alecdwm/LuaAsteroids/issues)*
 
 ## License
-[![Creative Commons License](https://i.creativecommons.org/l/by-nc-sa/4.0/88x31.png)](http://creativecommons.org/licenses/by-nc-sa/4.0/)  
+[![Creative Commons License](https://i.creativecommons.org/l/by-nc-sa/4.0/88x31.png)](http://creativecommons.org/licenses/by-nc-sa/4.0/)
 >This work is licensed under a [Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International License](http://creativecommons.org/licenses/by-nc-sa/4.0/).
 
 ## Credits
-[Atari](http://www.atari.com/)  
-[LÖVE2D](http://www.love2d.org)  
-[Helper Utilities for More Productivity](http://vrld.github.io/hump/)  
-[HardonCollider](https://github.com/vrld/HardonCollider)  
+[Atari](http://www.atari.com/)
+[LÖVE2D](http://www.love2d.org)
+[Helper Utilities for More Productivity](http://vrld.github.io/hump/)
+[HardonCollider](https://github.com/vrld/HardonCollider)
--- main.lua
+++ main.lua
@@ -2,73 +2,73 @@ debug = {}
 debug.enabled = true
 
 -- Libraries --
-class =				require "libraries.hump.class"
-vector =			require "libraries.hump.vector"
-HardonCollider =	require "libraries.hardoncollider"
+class =             require "libraries.hump.class"
+vector =            require "libraries.hump.vector"
+HardonCollider =    require "libraries.hardoncollider"
 
 -- Game Systems --
-audio =			require "systems.audio"
-debris =			require "systems.debris"
-npcship =			require "systems.npcship"
-player =			require "systems.player"
-projectiles =		require "systems.projectiles"
-states =			require "systems.states"
+audio =             require "systems.audio"
+debris =            require "systems.debris"
+npcship =           require "systems.npcship"
+player =            require "systems.player"
+projectiles =       require "systems.projectiles"
+states =            require "systems.states"
 
 -- Callbacks --
 function love.load()
-	love.graphics.setBackgroundColor(0,0,0)
-	math.randomseed(os.time())
+    love.graphics.setBackgroundColor(0,0,0)
+    math.randomseed(os.time())
 
-	states.load()
+    states.load()
 end
 
 function love.update(dt)
-	states.update(dt)
+    states.update(dt)
 end
 
 function love.draw()
-	states.draw()
+    states.draw()
 end
 
 function love.mousepressed(x,y,button)
-	states.mousepressed(x,y,button)
+    states.mousepressed(x,y,button)
 end
 
 function love.mousereleased(x,y,button)
-	states.mousereleased(x,y,button)
+    states.mousereleased(x,y,button)
 end
 
 function love.mousefocus(focus)
-	states.mousefocus(focus)
+    states.mousefocus(focus)
 end
 
 function love.keypressed(key,isrepeat)
-	if key == "escape" or key == "q" then
-		love.event.quit()
-	end
-	states.keypressed(key,isrepeat)
+    if key == "escape" or key == "q" then
+        love.event.quit()
+    end
+    states.keypressed(key,isrepeat)
 end
 
 function love.keyreleased(key)
-	states.keyreleased(key)
+    states.keyreleased(key)
 end
 
 function love.textinput(text)
-	states.textinput(text)
+    states.textinput(text)
 end
 
 function love.focus(focus)
-	states.focus(focus)
+    states.focus(focus)
 end
 
 function love.visible(visible)
-	states.visible(visible)
+    states.visible(visible)
 end
 
 function love.resize(w,h)
-	states.resize(w,h)
+    states.resize(w,h)
 end
 
 function love.quit()
-	states.quit()
+    states.quit()
 end
